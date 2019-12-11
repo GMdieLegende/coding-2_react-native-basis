a33f0786430bcbec245af9329a8425c82246915e
GMdieLegende <57195086+GMdieLegende@users.noreply.github.com>
2019-12-11 16:38:03 +0100

    added new files

--- App.js
+++ App.js
@@ -1,40 +1,54 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
+import ListView from './components/ListView';
+import { Slider } from 'react-native-elements'; 
+  /*
+   TodoApp: Haupteinstiegpunkt
+     render -> ListView wird angezeigt
+  */
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default class App extends React.Component {
+class TodoApp extends Component {
   render() {
     return (
+    
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Aufgaben:
+      <Text>
+  Whats to do?
         </Text>
-        <Card>
-          <AssetExample />
-        </Card>
+        
+        <ListView></ListView>
+        
+        <Image style={styles.logo} source={require('./assets/snack-icon.png')} />
       </View>
     );
   }
 }
 
+
+
+
+/*
+ Definition der CSS-Styles
+   im Objekt "styles" 
+*/
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    paddingRight: 2,
+    backgroundColor: '#F8F8F8',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  logo: {
+    height: 128,
+    width: 128,
+    justifyContent: 'center',
+    padding: 5
+  }
+  
 });
+
+export default TodoApp
